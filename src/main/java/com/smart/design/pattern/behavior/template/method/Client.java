312a2922e980903fdc9ca1d93056d77940a199de
liujunhua <msnetboy@outlook.com>
2020-09-04 18:48:44 +0800

    构建器模式完善

---
+++ src/main/java/com/smart/design/pattern/behavior/template/method/Client.java
@@ -0,0 +1,17 @@
+package com.smart.design.pattern.behavior.template.method;
+
+/**
+ * @author liujunhua
+ * @description: TODO
+ * @date 2020/9/28:33
+ */
+public class Client {
+    public static void main(String[] args) {
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                super.run();
+            }
+        };
+    }
+}
---
+++ src/main/java/com/smart/design/pattern/creation/builder/Product.java
@@ -0,0 +1,9 @@
+package com.smart.design.pattern.creation.builder;
+
+/**
+ * @author liujunhua
+ * @description: TODO
+ * @date 2020/9/417:12
+ */
+public class Product {
+}

22ebadb23115301a1cf1d21dda0e08505571aec6
刘君华 <msnetboy@outlook.com>
2020-09-13 13:34:58 +0800

    单例模式

--- src/main/java/com/smart/design/pattern/creation/factory/simple/Client.java
+++ src/main/java/com/smart/design/pattern/creation/factory/simple/Client.java
@@ -3,7 +3,7 @@ package com.smart.design.pattern.creation.factory.simple;
 public class Client {
     public static void main(String[] args) {
 
-        Product product = (Product) ProductFactory.GetProduct("A");//工厂类创建对象
+        Product product = (Product) ProductFactory.GetProduct("B");//工厂类创建对象
         product.commonMethod();
         product.specialMethod();
     }
